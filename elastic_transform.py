3821712184785662b6595b336c34e3fd2f97448e
simonjuleseric2 <simonburgermeister@simonburgermeisters-MacBook-Pro.local>
2020-03-04 22:50:48 +0100

    new Commit
    change to account for spreading of transformation over time

--- elastic_transform.py
+++ elastic_transform.py
@@ -7,9 +7,7 @@ import cv2
 def elastic_transf(img, alpha, sigma):
 
     random_state = np.random.RandomState(None)
-
     shape = img.shape
-
     dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
     dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
     if len(shape)>2:
@@ -20,7 +18,7 @@ def elastic_transf(img, alpha, sigma):
     else:
         x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]))
         indices = np.reshape(y+dy, (-1, 1)), np.reshape(x+dx, (-1, 1))
-        
+
     image = map_coordinates(img, indices, order=1, mode='reflect')
 
     return image.reshape(img.shape)
@@ -32,11 +30,11 @@ sigma=10
 
 imgx = cv2.imread('photo.jpg')
 imgx = cv2.cvtColor(imgx, cv2.COLOR_BGR2RGB)
+imgx=cv2.resize(imgx, (300, 300), interpolation = cv2.INTER_CUBIC)
 
-for i in range(1, 2500, 100):
-    frame=elastic_transf(imgx, i, sigma)
-    print(np.shape(frame))
-    ims.append(frame)
+for i in range(1, 3000, 50):
+    imgx=elastic_transf(imgx, i, sigma)
+    ims.append(imgx)
 
 
 clip = ImageSequenceClip(ims, fps=25)
